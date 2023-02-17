15e278f8f6b76109afb42a1f694ee7e8ebc90fc7
Douglas Arthur <douglasarthur.s.bento@gmail.com>
2023-02-17 10:01:55 -0400

    test que deu errado

---
+++ pages/eventos/script.js
@@ -0,0 +1,8 @@
+// let listEvents = [];
+// listEvents[0] = "../../images/Ex.webp";
+//
+//
+//
+// for(let i=0; i < listEvents.length ; i=i+1) {
+//     document.write("<img alt='img' src=" + listEvents[i] + " >")
+// }
