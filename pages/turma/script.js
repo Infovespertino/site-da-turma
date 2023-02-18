43033fb806121fe71f973c458f91d0e1a24fa335
Douglas bento <douglasarthur.s.bento@gmail.com>
2023-02-18 14:50:54 -0400

    atualização com nome e imagem

--- pages/turma/script.js
+++ pages/turma/script.js
@@ -1,63 +1,54 @@
-// // Cria um array com os caminhos das imagens
-// var imagens = ["../../images/feira-das-nacoes.jpeg", "../../images/semana-pra-vida.jpeg"];
-    
-// // Percorre o array de imagens e cria um elemento <img> para cada uma
-// for (var i = 0; i < imagens.length; i++) {
-//     var img = document.createElement("img");
-//     img.src = imagens[i];
-//     document.querySelector(".image").appendChild(img);
-// }
-
-
-// Cria um array com os caminhos das imagens e as classes correspondentes
-var imagens = [
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
-    {src: "../../images/giga.webp", class: "img"},
+const imagens = [
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
 ];
 
-// Percorre o array de imagens e cria um elemento <img> para cada uma
-for (var i = 0; i < imagens.length; i++) {
-    var img = document.createElement("img");
+for (let i = 0; i < imagens.length; i++) {
+    let img = document.createElement("img");
     img.src = imagens[i].src;
-    img.classList.add(imagens[i].class); // Adiciona a classe correspondente
-    document.querySelector(".image").appendChild(img);
+    img.alt = imagens[i].aluno;
+    let p = document.createElement("p");
+    p.innerText = imagens[i].aluno;
+    let divImg = document.createElement("div");
+    divImg.appendChild(img);
+    divImg.appendChild(p);
+    let div = document.querySelector(".image");
+    div.appendChild(divImg);
 }
