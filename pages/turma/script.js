8b41bf022d4372ebca7937c17e2d72fb71012d73
Douglas bento <douglasarthur.s.bento@gmail.com>
2023-02-19 13:16:25 -0400

    responsividade na pagina aplicada

--- pages/turma/script.js
+++ pages/turma/script.js
@@ -37,9 +37,10 @@ const imagens = [
     {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
     {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
     {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
-    {src: "../../images/giga.webp", aluno: "Nome Sobrenome"},
+    {src: "../../images/giga.webp", aluno: "Nome Sobrenome1"},
 ];
 
+//header-mobile
 for (let i = 0; i < imagens.length; i++) {
     let img = document.createElement("img");
     img.src = imagens[i].src;
@@ -53,73 +54,15 @@ for (let i = 0; i < imagens.length; i++) {
     div.appendChild(divImg);
 }
 
-if(window.innerWidth >= 320){
-    const mobile = document.getElementById("mobile")
 
+
+if(window.innerWidth > 500){
+    const headeMobile = document.getElementById("header-mobile")
+    headeMobile.remove()
+}
+else if(window.innerWidth <= 500){
     const header = document.getElementById("header")
     header.remove()
-
-    const headerMobile = document.createElement("nav")
-    headerMobile.classList.add("header-mobile")
-
-    const input = document.createElement("input")
-    input.type = "checkbox"
-    input.id = "check"
-
-    const label = document.createElement("label")
-    label.setAttribute("for", "check")
-    const imgSelect = document.createElement("img")
-    imgSelect.src = "../../images/select.svg"
-    label.appendChild(imgSelect)
-    
-    
-
-    const imgLogo = document.createElement("img")
-    imgLogo.src = "../../images/logo.svg"
-
-    const ul = document.createElement("ul")
-    const liEvents = document.createElement("li")
-    const liClass = document.createElement("li")
-    const liRedes = document.createElement("li")
-    const liHome = document.createElement("li")
-
-    const aHome = document.createElement("a")
-    aHome.href = "../../index.html"
-    const linkTextH = document.createTextNode("HOME");
-    aHome.appendChild(linkTextH);
-    liHome.appendChild(aHome)
-
-    const aEvents = document.createElement("a")
-    aEvents.href = "../eventos/index.html"
-    const linkTextE = document.createTextNode("EVENTOS");
-    aEvents.appendChild(linkTextE);
-    liEvents.appendChild(aEvents)
-    
-    const aClass = document.createElement("a")
-    aClass.href = "./index.html"
-    const linkTextC = document.createTextNode("TURMA");
-    aClass.appendChild(linkTextC);
-    liClass.appendChild(aClass)
-    
-    const aRedes = document.createElement("a")
-    aRedes.href = "./index.html"
-    const linkTextR = document.createTextNode("REDES");
-    aRedes.appendChild(linkTextR);
-    liRedes.appendChild(aRedes)
-
-
-    ul.appendChild(liHome)
-    ul.appendChild(liEvents)
-    ul.appendChild(liClass)
-    ul.appendChild(liRedes)
-    
-    
-    headerMobile.appendChild(imgLogo)
-    headerMobile.appendChild(input)
-    headerMobile.appendChild(label)
-    
-    headerMobile.appendChild(ul)
-
-
-    mobile.appendChild(headerMobile)
 }
+
+//header-mobile
