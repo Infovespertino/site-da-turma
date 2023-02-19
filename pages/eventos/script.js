7eb37a5079a182a2782c44135fd21d37d751b797
Douglas bento <douglasarthur.s.bento@gmail.com>
2023-02-19 01:01:56 -0400

    Conclusão pagina eventos

--- pages/eventos/script.js
+++ pages/eventos/script.js
@@ -6,3 +6,25 @@
 // for(let i=0; i < listEvents.length ; i=i+1) {
 //     document.write("<img alt='img' src=" + listEvents[i] + " >")
 // }
+
+const imagens = [
+    {src: "../../images/Ex.webp", aluno: "Nome Evento"},
+    {src: "../../images/Ex.webp", aluno: "Nome Evento"},
+    {src: "../../images/Ex.webp", aluno: "Nome Evento"},
+    {src: "../../images/Ex.webp", aluno: "Nome Evento"},
+    {src: "../../images/Ex.webp", aluno: "Nome Evento"},
+    {src: "../../images/Ex.webp", aluno: "Nome Evento"},
+];
+
+for (let i = 0; i < imagens.length; i++) {
+    let img = document.createElement("img");
+    img.src = imagens[i].src;
+    img.alt = imagens[i].aluno;
+    let p = document.createElement("p");
+    p.innerText = imagens[i].aluno;
+    let divImg = document.createElement("div");
+    divImg.appendChild(img);
+    divImg.appendChild(p);
+    let div = document.querySelector(".image");
+    div.appendChild(divImg);
+}
